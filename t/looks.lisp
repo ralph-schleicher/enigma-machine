795d5d36289bfe69377cc18d2709f31c14d92e3b
Ralph Schleicher <rs@ralph-schleicher.de>
2021-11-14 13:47:14 +0000

    New file.

---
+++ t/looks.lisp
@@ -0,0 +1,8 @@
+;; Message from Kapt. Lt. Hartwig Looks, U-264.
+(configure (make-enigma "M4")
+	   :umkehrwalze "B"
+	   :walzenlage '("β" "II" "IV" "I")
+	   :ringstellung "AAAV"
+	   :grundstellung "VJNA"
+	   :steckerverbindungen '("AT" "BL" "DF" "GJ" "HM"
+				  "NW" "OP" "QY" "RZ" "VX"))
